4f1b00374d96048673cb4b0fd1e040bd2c283dfe
liyitong <roseLi8664@163.com>
2019-03-27 15:56:42 +0800

    this is a first project

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrap-index">
+        1111111
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style lang="less" rel="stylesheet/less">
+
+</style>
